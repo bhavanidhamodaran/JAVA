d211751b11d3458c7406ae1d9fe605ed52c1441d
bhavanidhamodaran <40755920+bhavanidhamodaran@users.noreply.github.com>
2018-08-10 21:42:53 +0530

    Add files via upload

---
+++ ex12.java
@@ -0,0 +1,23 @@
+public class Main
+
+{
+
+	public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a,b,c,d;
+
+		System.out.println("Enter three numbers");
+
+		a = in.nextInt();
+
+		b = in.nextInt();
+
+		c = in.nextInt();
+
+		d = (a+b+c)/3;
+
+    	System.out.println("Average" +d);
+
+    	}
---
+++ ex5.java
@@ -0,0 +1,21 @@
+public class Main
+
+{
+
+	public static void main(String[] args) {
+
+		int a,b,c;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter two numbers");
+
+		a = in.nextInt();
+
+		b = in.nextInt();
+
+		c =a*b;
+
+		System.out.println(c);
+
+	}
